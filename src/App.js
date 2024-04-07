d2bf51e947073c25c5f0224563a62ed978c8922e
Sushma Nandiyawar <sushmanandiyawar@Sushmas-MacBook-Air.local>
2024-04-07 15:46:43 -0400

    dependencies

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/Sushma-Portfolio/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
